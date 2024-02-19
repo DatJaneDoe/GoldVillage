31ef0d08350b6c18b6c65386e6f69ad61d1218c1
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2024-02-19 05:04:19 -0500

    Added ModHelperData

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+namespace GoldVillage;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "41.0";
+    public const string Version = "1.1.0";
+    public const string Name = "GoldVillage";
+
+    public const string Description = "Replicates most vanilla buffs in the game and provides them all at once";
+
+    public const string Icon = "";
+    public const string RepoOwner = "DatJaneDoe";
+	
+}
